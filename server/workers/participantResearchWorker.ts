78c13d97a5058aa53a6065b400afd3c814e598ce
subhash <subhash@sanyaas.org>
2026-02-23 20:39:34 +0530

    update prompt for research

--- server/workers/participantResearchWorker.ts
+++ server/workers/participantResearchWorker.ts
@@ -19,7 +19,11 @@ export const participantResearchWorker = async (participant: Participant) => {
 
   const response = await ai.models.generateContent({
     model: "gemini-3-flash-preview",
-    contents: `i run a startup in personal finance space. i have a meeting with "${participant.name}" who is a partner at "${participant.organization}". tell me all you can find about him and what kind of startups they invest in and any other information i should know before i meet him`,
+    contents: `i run a startup in personal finance space. i have a meeting with  "${participant.name}" who is a partner at "${participant.organization}". 
+
+gather information about them and their company from their website and blogs, youtube videos and podcasts where they have appeared
+
+try to find as many sources as you can and then summarise all of them into points for me to understand their investment philosophy and how i should pitch to them`
   });
   console.log(response);
 
